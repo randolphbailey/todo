cefd4e7bcf24d8ab91402aaff61b96f5e8e8ffad
randolphbailey <randolphbailey@gmail.com>
2020-01-06 17:17:17 -0500

    fini

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const deleteTodo = i => {
+    let newList = todos;
+    newList.splice(i, 1);
+    setTodos([...newList]);
+  };
+
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <ul>
+            {todos.map((val, i) => (
+              <li key={i} onClick={e => deleteTodo(i)}>
+                {val}
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <form className="form" onSubmit={e => addTodo(e)}>
+            <input
+              type="text"
+              className="form-control"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
